45ca011a9694a870786ed3ee9c52ea458b3d9be0
Serhii Matviienko <smatvienko@outlook.com>
2019-11-13 22:57:15 +0100

    Feature: add initial frontend app structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Polital (currently empty page mode)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
